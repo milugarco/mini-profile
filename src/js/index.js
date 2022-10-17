269232766df4852e89ef341a7d557f8fd591f696
unknown <nosapien2@gmail.com>
2022-10-17 11:07:14 -0300

    first

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const pages = document.querySelectorAll(".page");
+
+pages.forEach(page => {
+    page.addEventListener("click", function () {
+
+        if (page.classList.contains("selected")) {
+            return;
+        }
+
+        selectedPage(page);
+        showInfoPage(page);
+    });
+});
+
+function selectedPage(page) {
+    const pageSelected = document.querySelector(".page.selected");
+    pageSelected.classList.remove("selected")
+
+    page.classList.add("selected")
+
+    return;
+};
+
+function showInfoPage(page) {
+    const informationSelected = document.querySelector(".information.selected");
+    informationSelected.classList.remove("selected")
+
+    const idElementInformationPage = `info-${page.id}`;
+
+    const informationShow = document.getElementById(idElementInformationPage)
+
+    informationShow.classList.add("selected");
+
+    return;
+}
